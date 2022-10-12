0f2a8f2c6e2a4c02790d8b6aacc8a2325645d2cb
Jung Mee Park <jmpark@arizona.edu>
2022-10-12 11:41:31 -0700

    git

--- scripts/01-merge-data.R
+++ scripts/01-merge-data.R
@@ -1,3 +1,44 @@
 # read in CatCloud data
 # Jung Mee Park
 # 2022-23-09
+
+#### load libraries ####
+library("googleAnalyticsR")
+library("tidyverse")
+library(readr)
+library(data.table)
+library(dplyr)
+library(tibble)
+library(tidyr)
+library(stringi)
+library(stringr)
+library(ggplot2)
+#### read in data ####
+getwd()
+
+
+folder <- "/Users/jungmeepark/Documents/Trellis/CatCloud_dashboard/initial_data"      # path to folder that holds multiple .csv files
+file_list <- list.files(path=folder, pattern="*.csv") # create list of all .csv files in folder
+
+# read in each .csv file in file_list and create a data frame with the same name as the .csv file
+for (i in 1:length(file_list)){
+  assign(file_list[i], 
+         read.csv(paste(folder,"/", file_list[i], sep=''))
+  )}
+
+#### merge data and rename datafile ####
+SF_UAccess <- merge(x = `UAccess_Student Academic Information.csv`, y = SFContactData.csv,
+                    by.x = "Student.ID", by.y = "Emplid") 
+
+#### recode the NA ing Academic.Standing, Degree Checkout, Expected graduation term ####
+SF_UAccess[SF_UAccess == "-"] <- NA  
+
+#### count the NA ####
+colSums(is.na(SF_UAccess))
+
+#### create a concatenated column with EDS affiliations
+# split EDS affiliations with ; separator
+
+SF_UAccess[c('eds1', 'eds2', 'eds3', 'eds4', 'eds5')] <- str_split_fixed(SF_UAccess$EDS.Affiliations, ";", 5)
+
+
--- scripts/02-visualizations.R
+++ scripts/02-visualizations.R
@@ -2,17 +2,60 @@
 # data visualizations
 # 2022-09-26
 
+library(data.table)
+#### get the top N majors ####
+count_Majors <- table(SF_UAccess$Primary.Major.Plan)  
+
+print (count_Majors)
+
+SF_UAccess$Primary.Major.Plan <- factor(SF_UAccess$Primary.Major.Plan)
+levels(SF_UAccess$Primary.Major.Plan)
+
+####
+SF_UAccess %>%
+  group_by(Primary.Major.Plan) %>%
+  top_n(2, Cumulative.GPA)
+
 #### visualizations ####
-graph <- SF_UAccess %>%
-  group_by(Cumulative.GPA) %>% 
-  # mutate(Q4 = fct_infreq(Q4)) %>%
-  drop_na(Cumulative.GPA) %>% 
-  ggplot(aes(x = factor(Primary.Major.Plan)), y = ..count.., fill = EDS.Primary.Affiliation) +
-  geom_bar(aes(fill = )) +
-  # coord_flip() +
-  labs(x = "", y = "responses")
+# 
+# plot <- ggplot() +
+#   # Points for each car
+#   geom_point(data = SF_UAccess, mapping = aes(y = Cumulative.GPA, x = Primary.Major.Plan)) +
+#   # Vertical bars for the means
+#   geom_point(data = SF_UAccess %>% 
+#                # Group the data by brand then get means
+#                group_by(Primary.Major.Plan) %>% 
+#                mutate(mean_GPA = mean(Cumulative.GPA)), 
+#              # Specify aesthetics
+#              mapping = aes(y = Primary.Major.Plan, x = mean_GPA), 
+#              size = 10, color = 'red', shape = '|') 
+# plot <- ggplot(SF_UAccess, aes(Primary.Major.Plan, Cumulative.GPA)) +           # ggplot2 barplot with sum
+#   geom_point(stat = "identity")
+# 
+# print(plot)
 
-Q4graph <- Q4graph + labs(title = "How satisfied were you with your initial training in Trellis?",
-                          subtitle = "Question 4 - full data", fill = "Profile Name")
+d2 <- SF_UAccess %>%
+  count(Primary.Major.Plan) %>%
+  top_n(20) %>%
+  arrange(desc(n, Primary.Major.Plan)) %>%
+  mutate(Primary.Major.Plan = factor(Primary.Major.Plan, levels = unique(Primary.Major.Plan)))
 
-print(Q4graph)
+GPA_top20_major <- SF_UAccess %>%
+  filter(Primary.Major.Plan %in% d2$Primary.Major.Plan) %>%
+  mutate(Primary.Major.Plan = factor(Primary.Major.Plan, levels = levels(d2$Primary.Major.Plan))) %>%
+  ggplot(aes(x = Primary.Major.Plan, y = Cumulative.GPA, fill = Primary.Major.Plan)) +
+  geom_violin() +
+  ylab("Cumulative GPA") +
+  xlab("Majors") +
+  stat_summary(fun = "mean", geom = "point", shape = 8, size = 3, color = "midnightblue") +
+  theme(axis.text.x = element_text(angle = -70, hjust = 0, vjust = 0)) +
+  theme(legend.position="none")
+
+GPA_top20_major  <- GPA_top20_major  +  labs(title = "Violin plot of Top 20 majors", 
+                                     subtitle = "with mean GPA") 
+print(GPA_top20_major)
+
+
+#### all majors distribution ####
+ggplot(SF_UAccess, aes(Cumulative.GPA)) + 
+  geom_histogram(binwidth = 0.1) 
