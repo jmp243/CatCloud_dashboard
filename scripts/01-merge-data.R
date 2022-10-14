d8340b5fc3294e1242ba47a6d660e1cf56197c95
Jung Mee Park <jmpark@arizona.edu>
2022-10-14 08:02:49 -0700

    changes to merge

--- scripts/01-merge-data.R
+++ scripts/01-merge-data.R
@@ -14,7 +14,15 @@ library(tidyr)
 library(stringi)
 library(stringr)
 library(ggplot2)
+library(readxl)
+
 # library(readr)
+#### Write csv #### 
+write_named_csv <- function(x) 
+  write_csv(x, file = paste0(
+    "~/Documents/Trellis/CatCloud_dashboard/clean_data/",
+    deparse(substitute(x)),".csv"))
+
 #### read in data ####
 getwd()
 
@@ -34,8 +42,7 @@ names(`UAccess_Student Academic Information.csv`)
 # SF_UAccess <- merge(x = `UAccess_Student Academic Information.csv`, y = SFContactData.csv,
 #                     by.x = "Student.ID", by.y = "Emplid") 
 # 
-# #### recode the NA ing Academic.Standing, Degree Checkout, Expected graduation term ####
-# SF_UAccess[SF_UAccess == "-"] <- NA  
+
 # 
 # #### updated CatCloud APP instance ID matches useriD
 # #### count the NA ####
@@ -46,14 +53,71 @@ names(`UAccess_Student Academic Information.csv`)
 # 
 # SF_UAccess[c('eds1', 'eds2', 'eds3', 'eds4', 'eds5')] <- str_split_fixed(SF_UAccess$EDS.Affiliations, ";", 5)
 
-#### merge sfcontact data with google
-Cat_SF <- merge(x = Updated_CatCloud_Users.csv, y = sf_users_IDs_emails.csv,
-                    by.x = "App.instance.ID", by.y = "User.ID") 
+# change column names 
+Users_for_Add_or_Edit.csv <- rename(Users_for_Add_or_Edit.csv, Add.Event.Count = Event.count)
 
+#### merge updated catcloud users to goals and users add or edit ####
+Cat_goals <- left_join(Updated_CatCloud_Users.csv, Users_Goals.csv)
+Cat_add <- left_join(Cat_goals, Users_for_Add_or_Edit.csv)
+# Cat2_SF <- merge(x = Cat_SF, y = Users_for_Add_or_Edit.csv,
+#                  by.x = "App.instance.ID", by.y = "App.instance.ID")
+
+# Cat_SF <- merge(x = Updated_CatCloud_Users.csv, y = sf_users_IDs_emails.csv,
+#                     by.x = "App.instance.ID", by.y = "User.ID") 
+
+#### merge sfcontact data with google
+Cat_SF <- left_join(Cat_add, sf_users_IDs_emails.csv, by = c("App.instance.ID" = "User.ID"))
+
+# not_Cat_SF <- anti_join(Updated_CatCloud_Users.csv, sf_users_IDs_emails.csv, by.x = "App.instance.ID", by.y = "User.ID")
+not_Cat_SF <- anti_join(Updated_CatCloud_Users.csv, sf_users_IDs_emails.csv, by = c("App.instance.ID" = "User.ID"))
+
+# write_named_csv(not_Cat_SF)
 Cat_SF <- Cat_SF %>% 
-  select(App.instance.ID, Sessions, Engaged.sessions, Event.count, Views, First.Name, Last.Name,
+  select(App.instance.ID, Sessions, Engaged.sessions, Event.count, Views, Goals.Event.count, Add.Event.Count,
+         First.Name, Last.Name,
          Last.Login, Email)
 
-#### merge updated catcloud users to users add or edit ####
-Cat2_SF <- merge(x = Cat_SF, y = Users_for_Add_or_Edit.csv,
-                 by.x = "App.instance.ID", by.y = "App.instance.ID")
+#### merge all current students based on emails to CatSF ####
+# Cat_SF_Students <- merge(x = Cat_SF, y = `20221012_All Current Students_GPA_Major_forCC.csv`,
+#                          by.x = "Email", by.y = "UA.Email.Address") 
+Cat_SF_Students <- left_join(Cat_SF, `20221012_All Current Students_GPA_Major_forCC.csv`, 
+                             by = c("Email" = "UA.Email.Address"))
+
+Cat_SF_Students <- Cat_SF_Students %>% 
+  select(-Alternate.Email.Address, -Personal..Email.Address, -Academic.Load, -Academic.Standing, -Degree.Earned.Date)
+
+#### reorder the academic levels ####
+unique(Cat_SF_Students$Academic.Level...Beginning.of.Term)
+
+Cat_SF_Students$Academic.Level...Beginning.of.Term <- factor(Cat_SF_Students$Academic.Level...Beginning.of.Term, 
+                                                             levels=c("Freshman", "Sophomore", "Junior", "Senior", "Graduate",
+                                                                      "Masters", "Professional Year 1", "Professional Year 2",
+                                                                      "Professional Year 3", "Professional Year 4", "Doctoral"))
+names(Cat_SF_Students)
+names(`UAccess_Student Academic Information.csv`)
+
+#### recode the NA ing Academic.Standing, Degree Checkout, Expected graduation term ####
+Cat_SF_Students[Cat_SF_Students == "-"] <- NA
+
+
+# Cat2_SF_Students <- merge(x = Cat2_SF, y = Cat_SF_Students,
+#                           by.x = "App.instance.ID", by.y = "App.instance.ID")
+# 
+# Cat2_SF_Students <- Cat2_SF_Students %>% 
+#   select(-Stream.name, -Namespace.ID, -Email.x, -First.Name.x, -Last.Name.x, -Views.x,
+#          -Sessions.x, -Engaged.sessions.x, -Event.count.x, -Last.Login.x, -Term.GPA, -Degree.Earned.Date)
+
+
+write_named_csv(Cat_SF_Students)
+
+#### evaluate data ####
+Cat_SF_Students %>% 
+  # group_by(Academic.Level...Beginning.of.Term, Goals.Event.count) %>% 
+  count(Academic.Level...Beginning.of.Term, wt = Goals.Event.count) 
+
+Cat_SF_Students %>% 
+  # group_by(Academic.Level...Beginning.of.Term, Goals.Event.count) %>% 
+  count(Academic.Level...Beginning.of.Term, wt = Add.Event.Count) 
+
+
+
--- scripts/02-visualizations.R
+++ scripts/02-visualizations.R
@@ -3,16 +3,19 @@
 # 2022-09-26
 
 library(data.table)
+library(lattice)
+library(latticeExtra)      
+
 #### get the top N majors ####
-count_Majors <- table(SF_UAccess$Primary.Major.Plan)  
+count_Majors <- table(Cat_SF_Students$Primary.Major.Plan)  
 
 print (count_Majors)
 
-SF_UAccess$Primary.Major.Plan <- factor(SF_UAccess$Primary.Major.Plan)
-levels(SF_UAccess$Primary.Major.Plan)
+Cat_SF_Students$Primary.Major.Plan <- factor(Cat_SF_Students$Primary.Major.Plan)
+levels(Cat_SF_Students$Primary.Major.Plan)
 
 ####
-SF_UAccess %>%
+Cat_SF_Students %>%
   group_by(Primary.Major.Plan) %>%
   top_n(2, Cumulative.GPA)
 
@@ -34,14 +37,16 @@ SF_UAccess %>%
 # 
 # print(plot)
 
-d2 <- SF_UAccess %>%
+# top 20 undergraduate majors
+d2 <- Cat_SF_Students %>%
   count(Primary.Major.Plan) %>%
   top_n(20) %>%
   arrange(desc(n, Primary.Major.Plan)) %>%
   mutate(Primary.Major.Plan = factor(Primary.Major.Plan, levels = unique(Primary.Major.Plan)))
 
-GPA_top20_major <- SF_UAccess %>%
+GPA_top20_major <- Cat_SF_Students %>%
   filter(Primary.Major.Plan %in% d2$Primary.Major.Plan) %>%
+  filter(Career == "Undergraduate") %>% 
   mutate(Primary.Major.Plan = factor(Primary.Major.Plan, levels = levels(d2$Primary.Major.Plan))) %>%
   ggplot(aes(x = Primary.Major.Plan, y = Cumulative.GPA, fill = Primary.Major.Plan)) +
   geom_violin() +
@@ -49,7 +54,7 @@ GPA_top20_major <- SF_UAccess %>%
   xlab("Majors") +
   stat_summary(fun = "mean", geom = "point", shape = 8, size = 3, color = "midnightblue") +
   theme(axis.text.x = element_text(angle = -70, hjust = 0, vjust = 0)) +
-  theme(legend.position="none")
+  theme(legend.position="none") 
 
 GPA_top20_major  <- GPA_top20_major  +  labs(title = "Violin plot of Top 20 majors", 
                                      subtitle = "with mean GPA") 
@@ -57,5 +62,71 @@ print(GPA_top20_major)
 
 
 #### all majors distribution ####
-ggplot(SF_UAccess, aes(Cumulative.GPA)) + 
-  geom_histogram(binwidth = 0.1) 
+u_grads <- Cat_SF_Students %>% 
+  filter(Career == "Undergraduate")
+#### reorder undergrads ####
+# unique(u_grads$Academic.Level...Beginning.of.Term)
+# u_grads$Academic.Level...Beginning.of.Term <- factor(u_grads$Academic.Level...Beginning.of.Term, 
+#                                                      levels=c("Freshman", "Sophomore", "Junior", "Senior"))
+
+#### histogram for undergraduate GPA ####
+hist_ugrad <- ggplot(u_grads, aes(Cumulative.GPA, fill = Academic.Level...Beginning.of.Term)) + 
+  geom_histogram(color = "black", binwidth = 0.3) +
+  xlab("Cumulative GPA") +
+  ylab("Number of Students") +
+  labs(fill='Academic Level')
+
+hist_ugrad  <- hist_ugrad  +  labs(title = "Histogram of Undergraduate GPA") 
+hist_ugrad
+
+#### what do do about class app ####
+
+g <- ggplot(Cat_SF_Students[which(Cat_SF_Students$Goals.Event.count>0),]) + 
+  stat_summary(mapping = aes(x=Academic.Level...Beginning.of.Term, y=Goals.Event.count, fill = Career), fun = "sum", geom = "bar", 
+               na.rm = TRUE, inherit.aes = FALSE) +
+  ylab("Academic Level") +
+  xlab("Count of Goals Users") +
+  labs(title = "Academic Levels of Goal Setters", fill = "Academic Career") +
+  theme(legend.position = c(0.15, 0.75), axis.text.x = element_text(angle = -70, hjust = 0, vjust = 0)) 
+
+g 
+
+add_users <- ggplot(Cat_SF_Students[which(Cat_SF_Students$Add.Event.Count>0),]) + 
+  stat_summary(mapping = aes(x=Academic.Level...Beginning.of.Term, y=Add.Event.Count, fill = Career), fun = "sum", geom = "bar", 
+               na.rm = TRUE, inherit.aes = FALSE) +
+  ylab("Academic Level") +
+  xlab("Count of Add Users") +
+  labs(title = "Academic Levels of add or edit", fill = "Academic Career") +
+  theme(legend.position = c(0.15, 0.75), axis.text.x = element_text(angle = -70, hjust = 0, vjust = 0)) 
+
+add_users
+
+
+add_goal <- ggplot(Cat_SF_Students[which(Cat_SF_Students$Add.Event.Count>0 | Cat_SF_Students$Goals.Event.count>0),]) + 
+  stat_summary(mapping = aes(x=Academic.Level...Beginning.of.Term, y=Add.Event.Count, fill = Career), fun = "sum", 
+               geom = "bar",
+               na.rm = TRUE, inherit.aes = FALSE) +
+  stat_summary(mapping = aes(x=Academic.Level...Beginning.of.Term, y=Goals.Event.count), fun = "sum", 
+               # geom = "line", 
+               na.rm = TRUE, inherit.aes = FALSE) +
+  ylab("Academic Level") +
+  xlab("Count of Users") +
+  labs(title = "Academic Levels of Event Counts", fill = "Academic Career") +
+  theme(legend.position = c(0.15, 0.75), axis.text.x = element_text(angle = -70, hjust = 0, vjust = 0)) 
+
+add_goal
+
+add_goal <- add_goal +   scale_fill_manual(name = NULL,
+                               guide = "legend",
+                               values = c("Undergraduate" = "lightblue",
+                                          "Graduate" = "red",
+                                          "Law" = "darkblue",
+                                          "Pharmacy" = "orange",
+                                          "Goals.Event.count" = "black"
+                                          ),
+                               labels = c('Undergraduate',
+                                          'Graduate',
+                                          'Law',
+                                          'Pharmacy',
+                                          'Goals'))
+add_goal
